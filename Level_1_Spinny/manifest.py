b39a9b9fd53469640879a8830bd5ee855e565b8f
Brandon Blodget <brandon.blodget@gmail.com>
2018-10-03 18:26:12 -0700

    Moved total_size and num_files to start of manifest.

--- Level_1_Spinny/manifest.py
+++ Level_1_Spinny/manifest.py
@@ -11,8 +11,8 @@ Usage: manifest.py <dataset_dir>
 
  This script outputs a manifest of your
  Project Gutenberg dataset directory.
- The manifest includes total number of text files,
- the total size of all the text files in bytes,
+ The manifest includes the total size of all text files,
+ The number of text files in the dataset, 
  and a list of all the text files with their sizes.
  This output can be redirected to a file such as
  as "manifest.txt"
@@ -41,8 +41,12 @@ for root ,dirs ,files in os.walk(dataset_dir):
                 file_path = os.path.join(root,file)
                 file_size = os.path.getsize(file_path)
                 total_size = total_size + file_size
-                print(file_size, file_path)
+                file_list.append((file_size, file_path))
 
-print("num_files: ",num_files)
+# Print the manifest
 print("total_size: ",total_size)
+print("num_files: ",num_files)
+for finfo in file_list:
+    print(finfo[0], finfo[1])
+
 
