10bd0acf47883058daf879b4236907477d5c06b4
Brandon Blodget <brandon.blodget@gmail.com>
2018-10-04 20:51:41 -0700

    First working version of the find.py command. Yay!

--- Level_1_Spinny/find.py
+++ Level_1_Spinny/find.py
@@ -7,7 +7,6 @@ import os
 import argparse
 import hashlib
 
-STR_LEN = 19
 
 DESC = \
 """
@@ -18,6 +17,97 @@ DESC = \
   string whose MD5 hash matches the given hash.
 """
 
+class Find_Hash:
+    """
+    Utility class for finding a hash in a large text dataset.
+    """
+
+    STR_LEN = 19
+    BUF_LEN = 500
+
+    def __init__(self, manifest_, md5_hash_, md5_num_):
+        """
+        Args:
+            manifest_ (str) : Name of the manifest file
+            md5_hash_ (str) : The search hash, Hexidecimal string, 32 chars.
+            md5_num_ (int) : The search hash as int
+        """
+        # Verify argument types
+        assert isinstance(manifest_, str)
+        assert isinstance(md5_hash_, str)
+        assert isinstance(md5_num_, int)
+
+        # Store in object
+        self.manifest = manifest_
+        self.md5_hash = md5_hash_
+        self.md5_num = md5_num_
+
+        # Load the file_list
+        self._parseManifest()
+
+    def _parseManifest(self):
+        """
+        Parse the Manifest file and generate list of all the text files
+        """
+        self.file_list = []
+        with open(self.manifest, 'r') as fp:
+            for cnt, line in enumerate(fp):
+                if cnt==0:
+                    key, value = line.split()
+                    self.total_size = int(value)
+                elif cnt==1:
+                    key, value = line.split()
+                    self.num_files = int(value)
+                elif cnt>1:
+                    size_str, file_path = line.split()
+                    self.file_list.append((int(size_str),file_path))
+
+    def process(self, chunk):
+        """
+        Move characters one at a time from chunk
+        into self.buffer_hash. Once buffer_hash has size of STR_LEN
+        start md5_hash of buffer_hash. 
+        """
+        while len(chunk) > 0:
+            # Pop char from chunk
+            c = chunk[0]
+            chunk = chunk[1:]
+
+            # Add character to self.buffer_hash
+            if len(self.buffer_hash) < self.STR_LEN:
+                # buffer not full yet
+                self.buffer_hash = self.buffer_hash + c
+            else:
+                # buffer_hash is full, so calc MD5 hash
+                byte_str = self.buffer_hash.encode()
+                md5 = hashlib.md5()
+                md5.update(byte_str)
+                if md5.hexdigest() == self.md5_hash:
+                    print("MATCH: ",byte_str)
+                    sys.exit()
+
+                # buffer is full, so drop oldest char
+                # TODO : On last buffer of file need to
+                # process this last hash.
+                self.buffer_hash = self.buffer_hash[1:] + c
+
+    def run(self):
+        """
+        Search the dataset for md5_hash
+        """
+        for index, file_info in enumerate(self.file_list):
+            file_size, file_path = file_info
+            self.buffer_hash = ""
+            # NOTE : Use latin-1 encoding to map byte values directly
+            # to first 256 Unicode code points.  Generates
+            # no exceptions, unlike utf-8.
+            with open(file_path, mode="r", encoding="latin-1") as fp:
+                print(index, file_path)
+                while True:
+                    chunk = fp.read(self.BUF_LEN)
+                    if  chunk == "":
+                        break
+                    self.process(chunk)
 
 # Start running here at main
 if __name__ == "__main__":
@@ -44,8 +134,9 @@ if __name__ == "__main__":
         print("ERROR: md5_hash is not hexidecimal")
         sys.exit()
 
+    fhash = Find_Hash(args.manifest, args.md5_hash, md5_num)
+    fhash.run()
 
-    print("manifest: ",args.manifest)
-    print("md5_hash: ",args.md5_hash)
+    print("DONE")
 
 
