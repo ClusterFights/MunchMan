f24c4873beec7b6660046ffc6c4ec16a1dd9f82d
Brandon Blodget <brandon.blodget@gmail.com>
2018-10-15 21:49:00 -0700

    Started testbench for hash_op module.

--- Level_2_Ducky/md5/hash_op.v
+++ Level_2_Ducky/md5/hash_op.v
@@ -32,10 +32,12 @@ module hash_op #
     input wire en,
 
     input wire [31:0] a, b, c, d,
-    input wire [511:0] m_in,
+    // m is a 16th of the full message
+    // higher level code pass in the
+    // correct part for this "index"
+    input wire [31:0] m,
 
-    output reg [31:0] a_out, b_out, c_out, d_out,
-    output reg [511:0] m_out
+    output wire [31:0] a_out, b_out, c_out, d_out
 );
 
 /*
@@ -44,25 +46,21 @@ module hash_op #
 *****************************
 */
 
-// Break the message (m_in) into sixteen
-// 32-bit words m[j] 0 <= j <= 15
-wire [15:0] m [31:0];
+// XXX wire [31:0] f_out = f(index, b, c, d);
+wire [31:0] f_out; 
+
+assign f_out = f(index,b,c,d); //(b & c) | ((~b) & d);
 
 /*
 *****************************
-* Assignments
+* Functions
 *****************************
 */
 
-// Generate the assignments to break
-// message (m_in) into sixteen 32-bit words.
-genvar gi;
-generate
-for (gi=0; gi<16; gi=gi+1) begin: mesg_i
-    assign m[gi] = m_in[32*gi +: 32];
-end
-endgenerate
-
+assign a_out = a6;
+assign b_out = b6;
+assign c_out = c6;
+assign d_out = d6;
 
 /*
 *****************************
@@ -84,17 +82,10 @@ begin
 end
 endfunction
 
-function[31:0] g;
-input [31:0] i;
+function[31:0] leftrotate;
+input [31:0] x, c;
 begin
-    if (i<16)
-        g = i;
-    else if (i<32)
-        g = (5*i + 1) % 16;
-    else if (i<48)
-        g = (3*i + 5) % 16;
-    else
-        g = (7*i) % 16;
+    leftrotate= (x<<c) | (x >> (32-c));
 end
 endfunction
 
@@ -109,9 +100,13 @@ reg [31:0] a1, b1, c1, d1;
 always @ (posedge clk)
 begin
     if (reset) begin
+        a1 <=- 0;
+        b1 <=- 0;
+        c1 <=- 0;
+        d1 <=- 0;
     end else begin
         if (en) begin
-            a1 <= a + f(index, b, c, d);
+            a1 <= a + f_out;
             b1 <= b;
             c1 <= c;
             d1 <= d;
@@ -120,20 +115,105 @@ begin
 end
 
 // Stage 2
+reg [31:0] a2, b2, c2, d2;
 always @ (posedge clk)
 begin
     if (reset) begin
+        a2 <=- 0;
+        b2 <=- 0;
+        c2 <=- 0;
+        d2 <=- 0;
     end else begin
         if (en) begin
-            a_out <= a1 + m[index];
-            b_out <= b1;
-            c_out <= c1;
-            d_out <= d1;
+            a2 <= a1 + m;
+            b2 <= b1;
+            c2 <= c1;
+            d2 <= d1;
 
-            m_out <= m_in;
         end
     end
 end
+
+// Stage 3
+reg [31:0] a3, b3, c3, d3;
+always @ (posedge clk)
+begin
+    if (reset) begin
+        a3 <=- 0;
+        b3 <=- 0;
+        c3 <=- 0;
+        d3 <=- 0;
+    end else begin
+        if (en) begin
+            a3 <= a2 + k;
+            b3 <= b2;
+            c3 <= c2;
+            d3 <= d2;
+
+        end
+    end
+end
+
+// Stage 4
+reg [31:0] a4, b4, c4, d4;
+always @ (posedge clk)
+begin
+    if (reset) begin
+        a4 <=- 0;
+        b4 <=- 0;
+        c4 <=- 0;
+        d4 <=- 0;
+    end else begin
+        if (en) begin
+            a4 <= leftrotate(a3,s);
+            b4 <= b3;
+            c4 <= c3;
+            d4 <= d3;
+
+        end
+    end
+end
+
+// Stage 5
+reg [31:0] a5, b5, c5, d5;
+always @ (posedge clk)
+begin
+    if (reset) begin
+        a5 <=- 0;
+        b5 <=- 0;
+        c5 <=- 0;
+        d5 <=- 0;
+    end else begin
+        if (en) begin
+            a5 <= a4 + b4;
+            b5 <= b4;
+            c5 <= c4;
+            d5 <= d4;
+
+        end
+    end
+end
+
+// Stage 6
+reg [31:0] a6, b6, c6, d6;
+always @ (posedge clk)
+begin
+    if (reset) begin
+        a6 <=- 0;
+        b6 <=- 0;
+        c6 <=- 0;
+        d6 <=- 0;
+    end else begin
+        if (en) begin
+            a6 <= d5;
+            b6 <= a5;
+            c6 <= b5;
+            d6 <= c5;
+
+        end
+    end
+end
+
 
 endmodule
 
