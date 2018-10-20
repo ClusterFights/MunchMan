fd92f8a0383b043107e48111d60b3ac43776e627
Brandon Blodget <brandon.blodget@gmail.com>
2018-10-20 11:34:55 -0700

    Finished state-machine.  No testing yet.

--- Level_2_Ducky/md5_impl/cmd_parser.v
+++ Level_2_Ducky/md5_impl/cmd_parser.v
@@ -9,18 +9,14 @@
 * commands:
 *
 * 1. Set target hash.
-* 2. Receive 'n' characters for md5 processing.
-* 3. Process 'n' characters for md5 processing.
+* 2. Process 'n' characters for md5 processing.
+* 3. Return match position and string.
 *
-* The difference between 2 and 3, is that three
-* makes sure that the 'n' characters have made
-* it all the way through the md5 pipeline before
-* returning.  While type 2 just makes sure that
-* 'n' characters have gotten into the the pipleline.  
-* Type three is good for processing the last
-* data chunk.
+* Multi-byte parameters and return values are
+* send MSB first (big endian/network order).
 *
 * Target Board: iCE40HX-8K Breakout Board.
+* Status: In development.
 *
 * Author : Brandon Bloodget
 *
@@ -35,14 +31,37 @@ module cmd_parser
     input wire clk_96mhz,
     input wire reset,
 
+    // uart_rx (receive)
     input wire [7:0] rxd_data,
     input wire rxd_data_ready,
 
-    output reg [7:0] txd_data,
+    // uart_tx (transmit)
+    input wire txd_busy,
     output reg txd_start,
-    output reg [7:0] leds
+    output reg [7:0] txd_data,
+
+    // char_buff (process)
+    input wire proc_done,
+    input wire proc_match,
+    input wire [15:0] proc_byte_pos,
+    input wire [7:0] proc_match_char,
+    output reg proc_start,
+    output reg [15:0] proc_num_bytes,
+    output reg [7:0] proc_data,
+    output reg proc_data_valid,
+    output reg proc_match_char_next,
+
+    // feedback/debug
+    output wire [7:0] leds
 );
 
+/*
+*****************************
+* Assignments
+*****************************
+*/
+
+assign leds[7:0] = cmd_state[7:0];
 
 /*
 *****************************
@@ -53,14 +72,18 @@ module cmd_parser
 // States
 localparam IDLE         = 0;
 localparam SET_HASH     = 1;
-localparam RECV_CHARS   = 2;
-localparam PROC_CHARS   = 3;
-localparam ACK          = 4;
+localparam PROC_CHARS1  = 2;
+localparam PROC_CHARS2  = 3;
+localparam PROC_CHARS3  = 4;
+localparam RET_CHARS1   = 5;
+localparam RET_CHARS2   = 5;
+localparam ACK          = 6;
+localparam NACK         = 7;
 
 // Character constants
 localparam SET_CMD      = 8'h01;
-localparam RECV_CMD     = 8'h02;
-localparam PROC_CMD     = 8'h03;
+localparam PROC_CMD     = 8'h02;
+localparam RET_CMD      = 8'h03;
 
 // Character constants
 localparam NACK_CHAR    = 8'h00;
@@ -72,36 +95,48 @@ localparam ACK_CHAR     = 8'h01;
 *****************************
 */
 
-reg [2:0] cmd_state;
+reg [7:0] cmd_state;
 reg [15:0] char_count;
-always (@posedge clk_96mhz)
+reg [127:0] target_hash;
+reg [15:0] num_bytes;
+always @ (posedge clk_96mhz)
 begin
     if (reset) begin
         cmd_state   <= IDLE;
-        leds        <=0;
+            proc_data <= 0;
+            proc_data_valid <= 0;
         char_count  <= 0;
         txd_data <= NACK_CHAR;
         txd_start <= 0;
+        target_hash <= 0;
+        proc_data <= 0;
+        proc_data_valid <= 0;
+        proc_start <= 0;
+        proc_num_bytes <= 0;
+        proc_match_char_next <= 0;
     end else begin
         case (cmd_state)
             IDLE : begin
-                leds <= 1;
                 char_count <= 0;
                 txd_data <= NACK_CHAR;
                 txd_start <= 0;
+                proc_data <= 0;
+                proc_data_valid <= 0;
+                proc_start <= 0;
+                proc_num_bytes <= 0;
+                proc_match_char_next <= 0;
                 // Waiting for a command byte
                 if (rxd_data_ready) begin
                     if (rxd_data == SET_CMD) begin
                         cmd_state <= SET_HASH;
-                    end else if (rxd_data == RECV_CMD) begin
-                        cmd_state <= RECV_CHARS;
                     end else if (rxd_data == PROC_CMD) begin
-                        cmd_state <= PROC_CHARS;
+                        cmd_state <= PROC_CHARS1;
+                    end else if (rxd_data == RET_CMD) begin
+                        cmd_state <= RET_CHARS1;
                     end
                 end
             end
             SET_HASH : begin
-                leds <= 2;
                 if (rxd_data_ready) begin
                     target_hash[127:0] <= {target_hash[119:0],rxd_data};
                     char_count <= char_count + 1;
@@ -110,18 +145,92 @@ begin
                     end
                 end
             end
-            RECV_CHARS : begin
-                leds <= 3;
+            PROC_CHARS1 : begin
+                // Read the number of bytes
+                if (rxd_data_ready) begin
+                    num_bytes[15:0] <= {num_bytes[7:0],rxd_data};
+                    char_count <= char_count + 1;
+                    if (char_count == 1) begin
+                        char_count <= 0;
+                        proc_num_bytes <= num_bytes;
+                        proc_start <= 1;
+                        cmd_state <= PROC_CHARS2;
+                    end
+                end
             end
-            PROC_CHARS : begin
-                leds <= 4;
+            PROC_CHARS2 : begin
+                proc_start <= 0;
+                // Read in the characters to process.
+                if (rxd_data_ready) begin
+                    proc_data <= rxd_data;
+                    proc_data_valid <= 1;
+                    char_count <= char_count + 1;
+                    if (char_count == (num_bytes-1)) begin
+                        proc_data_valid <= 0;
+                        cmd_state <= PROC_CHARS3;
+                    end
+                end else begin
+                    proc_data_valid <= 0;
+                end
+            end
+            PROC_CHARS3 : begin
+                // Wait for hash to complete.
+                if (proc_done) begin
+                    if (proc_match) begin
+                        cmd_state <= ACK;
+                    end else begin
+                        cmd_state <= NACK;
+                    end
+                end
+            end
+            RET_CHARS1 : begin
+                // Return match byte position
+                if (!txd_busy) begin
+                    txd_data <= (char_count==0) ? proc_byte_pos[15:8] :
+                        proc_byte_pos[7:0];
+                    txd_start <= 1;
+                    char_count <= char_count + 1;
+                    if (char_count == 1) begin
+                        char_count <= 0;
+                        cmd_state <= RET_CHARS2;
+                    end
+                end else begin
+                    txd_start <= 0;
+                end
+            end
+            RET_CHARS2 : begin
+                // Return the matched string
+                if (!txd_busy) begin
+                    txd_data <= proc_match_char;
+                    proc_match_char_next <= 1;
+                    txd_start <= 1;
+                    txd_data <= ACK_CHAR;
+                    txd_start <= 1;
+                    char_count <= char_count + 1;
+                    if (char_count == 18) begin
+                        cmd_state <= IDLE;
+                    end
+                end else begin
+                    proc_match_char_next <= 0;
+                    txd_start <= 0;
+                end
             end
             ACK : begin
-                leds <= 5;
                 if (!txd_busy) begin
                     txd_data <= ACK_CHAR;
                     txd_start <= 1;
                     cmd_state <= IDLE;
+                end else begin
+                    txd_start <= 0;
+                end
+            end
+            NACK : begin
+                if (!txd_busy) begin
+                    txd_data <= NACK_CHAR;
+                    txd_start <= 1;
+                    cmd_state <= IDLE;
+                end else begin
+                    txd_start <= 0;
                 end
             end
             default : begin
@@ -131,9 +240,6 @@ begin
     end
 end
 
-
-
-
-
 endmodule
+
 
