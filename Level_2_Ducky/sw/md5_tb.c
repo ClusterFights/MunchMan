8734a89dd5188e15b8f7d3e4eeb4bdcca86da635
Brandon Blodget <brandon.blodget@gmail.com>
2018-11-09 08:53:22 -0800

    Flush out test some more,  debugging ...

--- Level_2_Ducky/sw/md5_tb.c
+++ Level_2_Ducky/sw/md5_tb.c
@@ -20,7 +20,10 @@ static unsigned char target_hash[] = {
     0xa0, 0xfc, 0x9e, 0xe5
 };
 
-static unsigned char test_str[] = "Hello. The quick brow fox jumps over the lazy dog.";
+static unsigned char test_str[] = "Hello. The quick brown fox jumps over the lazy dog.";
+
+// test_str length is 51 = 0x33.
+static unsigned char test_str_len[] = {0x00, 0x33};
 
 static unsigned char ret_buffer[100] = {0};
 
@@ -47,18 +50,41 @@ int main(int argc, char *argv[])
     ftdi_setup(ftdi);
     printf("ftdi initialized\n");
 
-    // Send the target hash.
+    // Read anything that is hanging around.
+    printf("Clear input char queue.\n");
+    ret = ftdi_read_data(ftdi, ret_buffer, 100);
+    if (ret > 0) {
+        printf("WARN, read some data ret=%d\n",ret);
+    } else {
+        printf("OK. No data waiting.\n");
+    }
+    printf("press Enter\n");
+    getchar();
+
+    // Send the set hash command 0x01.
+    printf("Sending the set hash command 0x01.\n");
     ret = ftdi_write_data(ftdi, "\x01", 1); // cmd
     if (ret != 1)
     {
         printf("Error during ftdi_write_data: %d != %d\n",ret,1);
     }
+    printf("press Enter\n");
+    getchar();
+
+
+    // Send the target hash.
+    printf("Send the target hash.\n");
     ret = ftdi_write_data(ftdi, target_hash, 16); //data
     if (ret != 16)
     {
         printf("Error during ftdi_write_data: %d != %d\n",ret,16);
     }
+    printf("press Enter\n");
+    getchar();
+
+
     // Read the ACK.
+    printf("Read the ACK.\n");
     ack = ret_buffer[0];
     printf("before read. ack=%x\n",ack);
     ret = ftdi_read_data(ftdi, ret_buffer, 100);
@@ -68,8 +94,81 @@ int main(int argc, char *argv[])
     } else {
         printf("ERROR Read ACK. ret=%d, ack=%x\n",ret,ack);
     }
+    printf("press Enter\n");
+    getchar();
+
+    // Send the send data command 0x02.
+    printf("Sending the send data command 0x02.\n");
+    ret = ftdi_write_data(ftdi, "\x02", 1); // cmd
+    if (ret != 1)
+    {
+        printf("Error during ftdi_write_data: %d != %d\n",ret,1);
+    }
+    printf("press Enter\n");
+    getchar();
+
+    // Send the number of bytes to be sent.
+    printf("Send the number of bytes to be sent.\n");
+    ret = ftdi_write_data(ftdi, test_str_len, 2);
+    if (ret != 2)
+    {
+        printf("Error during ftdi_write_data: %d != %d\n",ret,16);
+    }
+    printf("press Enter\n");
+    getchar();
+
+    // Send the string.
+    printf("Sending the test string.\n");
+    ret = ftdi_write_data(ftdi, test_str, 51);
+    if (ret != 51)
+    {
+        printf("Error during ftdi_write_data: %d != %d\n",ret,16);
+    }
+    printf("press Enter\n");
+    getchar();
+
+    // Read the ACK.
+    printf("Read the ACK.\n");
+    ack = ret_buffer[0];
+    printf("before read. ack=%x\n",ack);
+    ret = ftdi_read_data(ftdi, ret_buffer, 100);
+    if (ret > 0) {
+        ack = ret_buffer[0];
+        printf("Read ACK. ret=%d, ack=%x\n",ret,ack);
+    } else {
+        printf("ERROR Read ACK. ret=%d, ack=%x\n",ret,ack);
+    }
+    printf("press Enter\n");
+    getchar();
+
+    // Send the command to read match data, 0x03.
+    printf("Sent command to read match data, 0x03.\n");
+    ret = ftdi_write_data(ftdi, "\x03", 1); // cmd
+    if (ret != 1)
+    {
+        printf("Error during ftdi_write_data: %d != %d\n",ret,1);
+    }
+    printf("press Enter\n");
+    getchar();
+
+    // Read the match data
+    printf("Read the match data.\n");
+    ret = ftdi_read_data(ftdi, ret_buffer, 100);
+    if (ret > 0) {
+        printf("Num of bytes read=%d\n",ret);
+        for (int i=0; i<ret; i++)
+        {
+            printf("%d: %x\n",i,ret_buffer[i]);
+        }
+    } else {
+        printf("ERROR ret=%d\n",ret);
+    }
+    printf("press Enter\n");
+    getchar();
+
 
     // Close connections
+    printf("Close the ftdi_usb.\n");
     ret = ftdi_usb_close(ftdi);
     if (ret < 0)
     {
