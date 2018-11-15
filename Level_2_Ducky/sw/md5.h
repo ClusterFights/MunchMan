aecd0dbd04d35eaddfce75c5c65bee4aed4e71a8
Brandon Blodget <brandon.blodget@gmail.com>
2018-11-14 20:32:12 -0800

    Move md5() into it's on .c and .h file.

---
+++ Level_2_Ducky/sw/md5.h
@@ -0,0 +1,43 @@
+/*
+ * FILE : md5.h
+ *
+ * Header file for the md5 function.
+ *
+ * AUTHOR : Brandon Blodget
+ * CREATE DATE: 11/14/2018
+ */
+
+#ifndef _MD5_H_
+#define _MD5_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+
+/*
+***************************
+* Macro
+***************************
+*/
+// leftrotate function definition
+#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+
+/*
+***************************
+* Function
+***************************
+*/
+int md5(uint8_t *initial_msg, uint8_t *hash_str);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
