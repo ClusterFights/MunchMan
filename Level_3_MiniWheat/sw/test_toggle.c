c5458edee5f28db21572a7d0385862da8664399d
Brandon Blodget <brandon.blodget@gmail.com>
2019-01-21 15:19:16 -0800

    Time 1000 toggles.
    It appears it toggles at 12-14Mhz.

--- Level_3_MiniWheat/sw/test_toggle.c
+++ Level_3_MiniWheat/sw/test_toggle.c
@@ -2,6 +2,7 @@
 test_toggle.c
 
 Toggles pin 21 on the rpi.
+Also connect a ground between rpi and ArtyS7.
 
 Compile:
  gcc $GPIO_BASE -Wall -o test_toggle test_toggle.c && sudo chown root test_toggle && sudo chmod +s test_toggle
@@ -11,6 +12,7 @@ Run:
 
 */
 #include <time.h>
+#include <sys/time.h>
 
 #define PI_GPIO_ERR  // errno.h
 #include "PI_GPIO.c" // stdio.h, sys/mman.h, fcntl.h, stdlib.h - signal.h
@@ -26,9 +28,12 @@ void sleep_ms(int ms)
 
 int main(int argc, char *argv[]) {
     int state=0;
+    int i;
+    struct timeval tv1, tv2;
     PI_GPIO_config(PI_OUT, BCM_GPIO_OUT);
 
-    while (1)
+    gettimeofday(&tv1, NULL);
+    for (i=0; i< 2000; i++)
     {
         if (state==1)
         {
@@ -40,7 +45,15 @@ int main(int argc, char *argv[]) {
             state=1;
         }
         // Sleep for .5 seconds
-        sleep_ms(500);
+        // sleep_ms(500);
     }
+    gettimeofday(&tv2, NULL);
+
+    double total_time = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+    (double) (tv2.tv_sec - tv1.tv_sec);
+    printf ("Total time = %f seconds\n", total_time);
+    double toggles_per_sec = 1000 / total_time;
+    printf("toggles_per_sec: %f\n",toggles_per_sec);
+
 }
 
