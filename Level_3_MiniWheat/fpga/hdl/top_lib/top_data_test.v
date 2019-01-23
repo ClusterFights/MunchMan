dc26b662842117b69a45b1b19256a72c4831e90a
Brandon Blodget <brandon.blodget@gmail.com>
2019-01-23 15:33:13 -0800

    Added a send test. From FPGA->rpi.

--- Level_3_MiniWheat/fpga/hdl/top_lib/top_data_test.v
+++ Level_3_MiniWheat/fpga/hdl/top_lib/top_data_test.v
@@ -6,7 +6,9 @@
 * over the parallel interface.  It expects
 * 256 sequential bytes, starting at 0.
 * After 256 bytes if they are all in order
-* it sends a 1 back to the RPI, otherwise a 0.
+* it turn on 1 green led (LD2) to indicate success.
+*
+* It then send 256 bytes back to the rpi.
 *
 * Author : Brandon Bloodget
 * Create Date : 1/21/2019
@@ -83,7 +85,12 @@ localparam SYNC2            = 2;
 localparam WAIT_CLOCK_LOW   = 3;
 localparam WAIT_CLOCK_HIGH  = 4;
 localparam CHECK            = 5;
-localparam DONE             = 6;
+localparam RECV_DONE        = 6;
+
+localparam SEND_WAIT_CLOCK_LOW   = 7;
+localparam SEND_WAIT_CLOCK_HIGH  = 8;
+localparam SEND_CHECK            = 9;
+localparam SEND_DONE             = 10;
 
 reg [3:0] state;
 reg [7:0] expected_val;
@@ -137,15 +144,45 @@ begin
                 end
                 if (expected_val == 255) begin
                     led_out[3:0] <= bus_data_out[3:0];
-                    state <= DONE;
+                    state <= RECV_DONE;
                 end else begin
                     expected_val <= expected_val + 1;
                     state <= WAIT_CLOCK_LOW;
                 end
             end
-            DONE : begin
-                state <= DONE;
+            RECV_DONE : begin
+                // Wait to switch to "Read" mode,
+                if (bus_rnw_reg == 1) begin
+                    bus_data_out <= 8'hFF; // setting to -1
+                    state <= SEND_WAIT_CLOCK_LOW;
+                end
             end
+
+            // send test: send bytes 0...255 back
+            SEND_WAIT_CLOCK_LOW : begin
+                if (bus_clk_reg == 0) begin
+                    // Set the data
+                    bus_data_out <= bus_data_out + 1;
+                    state <= SEND_WAIT_CLOCK_HIGH;
+                end
+            end
+            SEND_WAIT_CLOCK_HIGH : begin
+                if (bus_clk_reg == 1) begin
+                    state <= SEND_CHECK;
+                end
+            end
+            SEND_CHECK : begin
+                if (bus_data_out == 255) begin
+                    state <= SEND_DONE;
+                end else begin
+                    state <= SEND_WAIT_CLOCK_LOW;
+                end
+            end
+            SEND_DONE : begin
+                led_out[1] <= 1;
+                state <= SEND_DONE;
+            end
+
             default : begin
                 state <= IDLE;
             end
