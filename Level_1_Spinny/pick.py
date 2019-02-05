e3e68325a9ceae36e1e87f9f7b5773255e17d86f
Brandon Blodget <brandon.blodget@gmail.com>
2019-02-04 16:14:35 -0800

    Added -no_eol option to pick.py

--- Level_1_Spinny/pick.py
+++ Level_1_Spinny/pick.py
@@ -14,7 +14,7 @@ DESC =  \
  This script reads in a manifest.txt file
  that was created with manifest.py.  It
  then selects a file at random, then
- selects a 19 character string starting
+ selects a character string starting
  from a random byte offset.  It then calculates
  the MD5 hash of the string. It outputs
  the file index into the manifest, the file name,
@@ -26,67 +26,85 @@ DESC =  \
 # ./pick.py -i 439 -o 90560
 # ./pick.py -i 198 -o 55481
 
-# parse the command line arguments
-parser = argparse.ArgumentParser(description=DESC)
-parser.add_argument("manifest", nargs="?", help="The manifiest.txt file. "
-                   "Default is manifest.txt", default="manifest.txt")
-parser.add_argument("-i", "--index", type=int, help="Use this book index. "
-                    "Instead of random.")
-parser.add_argument("-o", "--offset", type=int, help="Use this file byte offset. "
-                    "Instead of random.")
-parser.add_argument("-s", "--str_len", type=int, help="Sets the str_len to search. "
-                    "Default is 19.", default=19)
-parser.parse_args()
-args = parser.parse_args()
+# Start running here at main
+if __name__ == "__main__":
 
-# Check the manifest file
-manifest = args.manifest
-if not os.path.isfile(manifest):
-    print("ERROR: Not a file: ",manifest)
-    sys.exit()
+    # parse the command line arguments
+    parser = argparse.ArgumentParser(description=DESC)
+    parser.add_argument("manifest", nargs="?", help="The manifiest.txt file. "
+                       "Default is manifest.txt", default="manifest.txt")
+    parser.add_argument("-i", "--index", type=int, help="Use this book index. "
+                        "Instead of random.")
+    parser.add_argument("-o", "--offset", type=int, help="Use this file byte offset. "
+                        "Instead of random.")
+    parser.add_argument("-s", "--str_len", type=int, help="Sets the str_len to search. "
+                        "Default is 19.", default=19)
+    parser.add_argument("-n", "--no_eol", help="Pick a string that does cross lines.",
+                        action="store_true")
+    parser.parse_args()
+    args = parser.parse_args()
 
-# Parse the manifest.
-file_list = []
-with open(manifest, 'r') as fp:
-    for cnt, line in enumerate(fp):
-        if cnt>1:
-            size_str, file_path = line.split()
-            file_list.append((int(size_str),file_path))
+    # Check the manifest file
+    manifest = args.manifest
+    if not os.path.isfile(manifest):
+        print("ERROR: Not a file: ",manifest)
+        sys.exit()
 
-# Select file from dataset
-num_files = len(file_list)
-if args.index is None:
-    index = random.randrange(num_files)
-else:
-    index = args.index
+    # Parse the manifest.
+    file_list = []
+    with open(manifest, 'r') as fp:
+        for cnt, line in enumerate(fp):
+            if cnt>1:
+                size_str, file_path = line.split()
+                file_list.append((int(size_str),file_path))
 
-# Get info about the file
-size = file_list[index][0]
-file_path = file_list[index][1]
+    # Select file from dataset
+    num_files = len(file_list)
+    if args.index is None:
+        index = random.randrange(num_files)
+    else:
+        index = args.index
 
-# Select random substring
-max_offset = size - args.str_len -1
-print("max_offset: {}".format(max_offset))
-if args.offset is None:
-    byte_offset = random.randint(0,max_offset)
-else:
-    byte_offset = args.offset
+    # Get info about the file
+    size = file_list[index][0]
+    file_path = file_list[index][1]
 
-# Extract the string
-# NOTE : Read file as binary using "rb"
-with open(file_path, mode="rb") as fp:
-    fp.seek(byte_offset)
-    byte_str = fp.read(args.str_len)
+    # compute max offset
+    max_offset = size - args.str_len -1
+    print("max_offset: {}".format(max_offset))
 
-# Compute the hash of byte_str
-md5 = hashlib.md5()
-md5.update(byte_str)
+    string_ok = False
+    while not string_ok:
 
-# Print selection
-print("file_index: {} out of {}".format(index,num_files))
-print("file_path: ",file_path)
-print("byte_offset: ",byte_offset)
-print('str_len: {}'.format(args.str_len))
-print('byte_str: {}'.format(byte_str))
-print('hash: {}'.format(md5.hexdigest()))
+        # Select random substring
+        if args.offset is None:
+            byte_offset = random.randint(0,max_offset)
+        else:
+            byte_offset = args.offset
+
+        # Extract the string
+        # NOTE : Read file as binary using "rb"
+        with open(file_path, mode="rb") as fp:
+            fp.seek(byte_offset)
+            byte_str = fp.read(args.str_len)
+
+        string_ok = True
+        if args.no_eol:
+            # Check if string has \n or \r
+            if b'\n' in byte_str or b'\r' in byte_str:
+                # print("  skip str: {}".format(byte_str))
+                string_ok = False
+
+
+    # Compute the hash of byte_str
+    md5 = hashlib.md5()
+    md5.update(byte_str)
+
+    # Print selection
+    print("file_index: {} out of {}".format(index,num_files))
+    print("file_path: ",file_path)
+    print("byte_offset: ",byte_offset)
+    print('str_len: {}'.format(args.str_len))
+    print('byte_str: {}'.format(byte_str))
+    print('hash: {}'.format(md5.hexdigest()))
 
